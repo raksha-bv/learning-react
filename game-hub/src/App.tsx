88c7c3555a83c5e6cbd19318ab30e9f2adc91c97
Raksha <rakshabv04@gmail.com>
2024-09-21 19:08:42 +0530

    implement sorting games

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -6,10 +6,12 @@ import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
+import SortSelector from "./components/SortSelector";
 
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
+  sortOrder: string;
 }
 
 function App() {
@@ -25,7 +27,7 @@ function App() {
         lg: "200px 1fr",
       }}
     >
-      <GridItem area="nav">
+      <GridItem area="nav" marginBottom={5} marginTop={2}>
         <NavBar />
       </GridItem>
       <Show above="lg">
@@ -38,12 +40,20 @@ function App() {
       </Show>
 
       <GridItem area="main">
-        <PlatformSelector
-          selectedPlatform={gameQuery.platform}
-          onSelectPlatform={(platform) =>
-            setGameQuery({ ...gameQuery, platform })
-          }
-        />
+        <HStack spacing={5} paddingLeft={2} marginBottom={5}>
+          <PlatformSelector
+            selectedPlatform={gameQuery.platform}
+            onSelectPlatform={(platform) =>
+              setGameQuery({ ...gameQuery, platform })
+            }
+          />
+          <SortSelector
+            onSelectSortOrder={(sortOrder) =>
+              setGameQuery({ ...gameQuery, sortOrder })
+            }
+            sortOrder={gameQuery.sortOrder}
+          />
+        </HStack>
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
     </Grid>
--- game-hub/src/services/image-url.ts
+++ game-hub/src/services/image-url.ts
@@ -1,4 +1,7 @@
+import noImg from "../assets/noImg.jpg";
+
 const croppedImageUrl = (url: string) => {
+  if (!url) return noImg;
   const target = "media/";
   const cropIndex = url.indexOf(target) + target.length;
   return url.slice(0, cropIndex) + "crop/600/400/" + url.slice(cropIndex);
